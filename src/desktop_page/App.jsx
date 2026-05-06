5eeeb156f158033f8d62458eb261d69d802c3eba
Justin Taylor <taylor15@rose-hulman.edu>
2026-05-06 13:57:18 -0400

    Some stuff

---
+++ src/desktop_page/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [testguy, alterTestguy] = useState('fish')
+
+  function doSomething(e) {
+    alterTestguy(e.target.value)
+  }
+
+  //<> is a React Fragment that doesn't include extra DOM elements
+  return (
+    <> 
+      <h1>Shopping List</h1>
+      <div>
+        <form>
+          <p>
+            <input type="text" id="text1" required pattern="\w[0-9a-zA-Z\- ]*"
+              title="Must have at least one non-whitespace character and no special characters."
+              onChange={doSomething}></input>
+          </p>
+          <p>
+            <input id="button1" type="button" value="Add Item To List" 
+              // TODO: With your instructor
+            ></input>
+          </p>
+        </form>
+      </div>
+      <div className="list-container">
+        <div className="list">
+          <section name="to_get">
+            <h2>To Get:</h2>
+            <ul id="list1">{
+              testguy
+            }
+            </ul>
+          </section>
+        </div>
+        <div className="list">
+          <section name="got">
+            <h2>Got:</h2>
+            <ul id="list2">{
+              // TODO: With your instructor
+            }
+            </ul>
+          </section>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
