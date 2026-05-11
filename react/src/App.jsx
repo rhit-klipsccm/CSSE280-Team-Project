3e722fd9eb415689e5361666e369e598ab662d0b
Justin Taylor <taylor15@rose-hulman.edu>
2026-05-11 14:34:26 -0400

    Server Decoration added

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -7,23 +7,35 @@ function App() {
   //<> is a React Fragment that doesn't include extra DOM elements
   return (
       <>
-        <iframe src="https://calendar.google.com/calendar/embed?src=357fce2b791dd6d8586930ba805fc98b311249542860748111772fc4bf67f4c8%40group.calendar.google.com&ctz=America%2FIndiana%2FIndianapolis"></iframe>
+        <h1 class="header">Calendar</h1>
 
-        <form method="POST" action="/requests" enctype="application/x-www-form-urlencoded">
+        <div class = "register-form">
+          <form method="POST" action="/requests" enctype="application/x-www-form-urlencoded" class="form">
           
-          <label for="name">Request Name:</label>
-          <p><input type="text" name="name" id="name" placeholder="Enter seven words or less"/></p>
+            <p><input type="text" name="name" id="name" placeholder="Enter Reservation Name"/></p>
 
-          <label for="date">Request Date:</label>
-          <p><input type="text" name="date" id="date"/></p>
+            <div class="time-container">
+              <div class="labelgroup-container">
+                <label for="date">Date:</label>
+                <p><input type="date" name="date" id="date"/></p>
+              </div>
 
-          <label for="start-time">Request Start Time:</label>
-          <p><input type="text" name="start-time" id="start-time"/></p>
-          
-          <label for="end-time">Request End Time:</label>
-          <p><input type="text" name="end-time" id="end-time"/></p>
-        
-        </form>
+              <div class="labelgroup-container">
+                <label for="start-time">Start Time:</label>
+                <p><input type="time" name="start-time" id="start-time"/></p>
+              </div>
+                
+              <div class="labelgroup-container">
+                <label for="end-time">End Time:</label>
+                <p><input type="time" name="end-time" id="end-time"/></p>
+              </div>
+            </div>
+
+            <p><input type="submit" name="submit-request" id="submit-request" value="Submit Reservation Request"/></p>
+          </form>
+
+          <iframe src="https://calendar.google.com/calendar/embed?src=357fce2b791dd6d8586930ba805fc98b311249542860748111772fc4bf67f4c8%40group.calendar.google.com&ctz=America%2FIndiana%2FIndianapolis" class="calendar"></iframe>
+        </div>
       </>
   )
 }
